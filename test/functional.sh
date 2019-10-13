9cb39023c574926e8b2e33d403b1f792af98a995
je9000 <none@example.com>
2019-10-13 04:27:03 -0700

    Simple functional test.

---
+++ test/functional.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+function finish {
+    if [ -n "$CARAFE_PID" ]; then
+        kill $CARAFE_PID
+    fi
+}
+function err {
+    echo "Error!" 1>&2
+    exit 1
+}
+trap finish EXIT
+trap err ERR
+
+../carafe &
+CARAFE_PID=$!
+curl -s 'http://127.0.0.1:8080/test' | grep 'Headers:'
+curl -s 'http://127.0.0.1:8080/var/var_here' | grep 'var_here'
+
+echo OK
